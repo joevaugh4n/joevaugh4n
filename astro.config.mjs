63e2629e8197c5d4ddf0ab25a172dc4f6a649182
Joe Vaughan <jbvaughan1993@gmail.com>
2024-10-26 11:37:54 +0100

    Styling updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+
 import react from "@astrojs/react";
 
 // https://astro.build/config
