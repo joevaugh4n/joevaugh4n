e16588d7f61762d732705eb101fa8e9914fd44bc
Joe Vaughan <jbvaughan1993@gmail.com>
2025-02-01 22:56:48 +0000

    first commit

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from "astro:content";
+
+// 2. Import loader(s)
+import { glob } from "astro/loaders";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/data/blog" }),
+  schema: z.object({
+    title: z.string(),
+    date: z.coerce.date().transform(
+      (date) => date.toISOString().split("T")[0] // Converts to 'YYYY-MM-DD'
+    ),
+    description: z.string(),
+  }),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { blog };
