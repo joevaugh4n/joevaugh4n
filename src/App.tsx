4e3a7feda5be51ccad98336c4e5927ad12dcc118
Joe Vaughan <jbvaughan1993@gmail.com>
2025-03-16 10:34:20 +0000

    styling update

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,14 @@ interface ButtonProps {
   onClickAction: (arg: React.ReactNode) => void;
   target: React.ReactNode;
   caption: string;
+  colour: string;
 }
 
-function Button({ onClickAction, target, caption }: ButtonProps) {
+function Button({ onClickAction, target, caption, colour }: ButtonProps) {
   return (
     <button
       onClick={() => onClickAction(target)}
-      className={`menu-item ${target ? "" : "active"}`}
+      className={`menu-item ${colour} ${target ? "" : "active"}`}
     >
       {caption}
     </button>
@@ -34,15 +35,21 @@ function SocialIcon({ url, title, icon }: IconProps) {
 }
 function App() {
   const [text, setText] = useState<React.ReactNode>("");
-  const career = (
-    <>
-      Currently, I'm the marketing lead at Museum of English Rural Life. I
-      previously worked at MERL and Reading Museum from 2019-2023. From
-      2023-2024 I was the first marketing hire at Storybook, one of the world's
-      biggest apps for web development. Before that, I was a copywriter for a
-      bunch of brands that you probably think about every day.
-    </>
-  );
+  const career = {
+    title: "Career",
+    body: (
+      <>
+        Currently, I'm the marketing lead at{" "}
+        <a href="https://merl.reading.ac.uk/">Museum of English Rural Life</a>.
+        I previously worked at MERL and{" "}
+        <a href="https://www.readingmuseum.org.uk/">Reading Museum</a> from
+        2019-2023. From 2023-2024 I was the first marketing hire at{" "}
+        <a href="https://storybook.js.org">Storybook</a>, one of the world's
+        most popular tools for app development. Before that, I was a copywriter
+        for a bunch of brands that you probably think about every day.
+      </>
+    ),
+  };
 
   const contact = (
     <>
@@ -72,15 +79,30 @@ function App() {
         />
       </nav>
       <h1>Joe Vaughan</h1>
-      <p>
-        Hey. I'm a marketer, writer, tinkerer, and wearer of many hats. I take
-        niche topics and make content showing why they're the most important
-        thing in the entire world.
+      <p className="intro">
+        Marketer, writer, tinkerer. Wearer of many hats. I take niche topics and
+        make content showing why they are the most important thing in the entire
+        world.
       </p>
       <nav className="menu">
-        <Button onClickAction={setText} target={career} caption="Career" />
-        <Button onClickAction={setText} target={contact} caption="Contact" />
-        <Button onClickAction={setText} target={website} caption="Website" />
+        <Button
+          onClickAction={setText}
+          target={career.body}
+          colour="vanilla"
+          caption="Career"
+        />
+        <Button
+          onClickAction={setText}
+          target={contact}
+          colour="orange"
+          caption="Contact"
+        />
+        <Button
+          onClickAction={setText}
+          target={website}
+          caption="Website"
+          colour="blue"
+        />
       </nav>
       {
         // Hide on mount
