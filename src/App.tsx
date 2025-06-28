4f7c82e817dba71abe1c26cf1c6673686cf12f7c
joe vaughan <jbvaughan1993@gmail.com>
2025-06-28 21:27:08 +0100

    remove baby

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import "./app.css";
-import BabyWithAGun from "/baby.jpg"
-import { useState } from "react";
 import Bluesky from "/bluesky.svg";
 import LinkedIn from "/linkedin.svg";
+import Joe from "/joe.jpg"
 
 interface ButtonProps {
   onClickAction: (arg: React.ReactNode) => void;
@@ -49,53 +48,6 @@ function SocialIcon({ url, title, icon }: IconProps) {
 }
 
 function App() {
-
-  interface TextOption {
-    /** The name. */
-    title: string;
-    /** React component. */
-    body: React.ReactNode;
-  }
-
-  const TextOptions: TextOption[] = [{
-    title: "Professional",
-    body: (
-      <><p>I take niche topics and show why they are <i>the</i> most important
-        thing in the entire world.</p>
-        <p>I run <a href="https://bsky.app/profile/themerl.bsky.social">The Museum of English Rural Life's very popular Bluesky account</a> and co-host and produce its podcast, <a href="https://merl.reading.ac.uk/explore/absolute-units/">Absolute Units</a>.</p>
-        <p>I have spoken about museum social media at international conferences (like BrightonSEO and the Museum + Heritage Show) and been interviewed by international press (including NPR, Polygon, The Guardian, and BBC Radio).</p><p>I divide my time between Reading and The Internet.</p>
-      </>
-    )
-  },
-  {
-    title: 'Comedian',
-    body: (
-      <>
-        <p>I'm a baby comedian and I have started doing gigs!</p>
-        <p> I'll list more here in the future (last updated 8 June 2025).</p>
-      </>
-    )
-  },
-  {
-    title: 'baby with a gun',
-    body: (
-      <><img src={BabyWithAGun} alt='baby with a gun' className='baby' />
-      </>
-    )
-  },
-  {
-    title: 'Contact',
-    body: (
-      <>
-        <a href="mailto:jbvaughan1993@gmail.com">email</a>
-      </>
-    )
-  }
-  ]
-
-  const [text, setText] = useState<React.ReactNode>(TextOptions[0].body);
-  const [active, setActive] = useState(0);
-
   return (
     <div className="container">
       <nav className="top-menu">
@@ -110,28 +62,13 @@ function App() {
           url="https://www.linkedin.com/in/joevaugh4n/"
         />
       </nav>
-      <header>
+      <header className='title'>
         <h1>Joe Vaughan</h1>
-        <p className="intro">
-          rest in peace pirates you would have loved hookup culture
-        </p>
-        <nav className="menu">
-          {TextOptions.map((option, i) => (
-            <Button
-              key={option.title}
-              onClickAction={setText}
-              setActive={setActive}
-              target={option.body}
-              caption={option.title}
-              index={i}
-              isActive={active === i}
-            />
-          ))}
-        </nav>
+        <p>Content lead at The Museum of English Rural Life. Comedian. <a href='mailto:jbvaughan1993@gmail.com'>Contact</a>.</p>
       </header>
-      {
-        text != "" && TextOptions[active].title.toLowerCase() != 'baby with a gun' ? <p className="highlight-text">{text}</p> : <div>{text}</div>
-      }
+
+      <img src={Joe} className='portrait' />
+
     </div>
   );
 }
