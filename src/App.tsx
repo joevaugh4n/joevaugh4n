b7daf862c78a823cbcb29e37e8ac7dfa9e05e09f
Joe Vaughan <jbvaughan1993@gmail.com>
2026-01-18 11:18:21 +0000

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,58 @@
 import "./app.css";
-import Bluesky from "/bluesky.svg";
-import LinkedIn from "/linkedin.svg";
-import YouTube from "/youtube.png";
-import { ShowList, Show } from "./ShowList";
-
-interface IconProps {
-  url: string;
-  title: string;
-  icon: string;
-}
-
-function SocialIcon({ url, title, icon }: IconProps) {
-  return (
-    <a href={url} title={title}>
-      <img src={icon} alt={title} className="social-icon" />
-    </a>
-  );
-}
 
 function App() {
   return (
     <div className="container">
-      <nav className="top-menu">
-        <SocialIcon
-          title="Bluesky"
-          icon={Bluesky}
-          url="https://bsky.app/profile/joevaughan.net"
-        />
-        <SocialIcon
-          title="LinkedIn"
-          icon={LinkedIn}
-          url="https://www.linkedin.com/in/joevaugh4n/"
-        />
-        <SocialIcon
-          title="YouTube"
-          icon={YouTube}
-          url="https://www.youtube.com/@joevaughanleaks"
-        />
-      </nav>
-      <header className="title">
-        <h1>Joe Vaughan</h1>
-        <p>
-          Content at The Museum of English Rural Life. Host of{" "}
-          <a href="https://merl.reading.ac.uk/explore/absolute-units/">
-            Absolute Units
-          </a>
-          . Comedian. <a href="mailto:jbvaughan1993@gmail.com">Contact</a>.
-        </p>
-      </header>
-
-      <hgroup className="shows">
-        <h2>Shows</h2>
-        <ul>
-          {ShowList.slice()
-            .sort((a, b) => a.date.getTime() - b.date.getTime())
-            .map((show) => (
-              <Show
-                key={show.id}
-                id={show.id}
-                title={show.title}
-                location={show.location}
-                date={show.date}
-              />
-            ))}
-        </ul>
-      </hgroup>
+      <p>
+        <h1 className="title">Joe Vaughan</h1> is a comedian and creative based
+        in London.
+      </p>
+      <p>He works at The Museum of English Rural Life, Reading.</p>
+      <p>
+        He is really glad you visited this website to learn more and slightly
+        embarassed about the state of the place.
+      </p>
+      <p>
+        If you would like to pull up a chair you can find a wide selection of
+        them{" "}
+        <a
+          href="https://www.google.com/search?sca_esv=651164c6481fa95a&rlz=1C5MACD_enGB1106GB1106&sxsrf=ANbL-n5lQg7m6QCdAMzbjhJYeIyS_bpCzw:1768735020801&udm=2&fbs=ADc_l-aN0CWEZBOHjofHoaMMDiKpaEWjvZ2Py1XXV8d8KvlI3o6iwGk6Iv1tRbZIBNIVs-5CylPOvMibOiu5QsvYkQPVaca9NoFh0i-99KVMPoRW1_5ZGfygFvQOnyogqV993ox9ylLTTUwfmG_761AL3HjRoY4R4gkx14P0EQw44J08x72_vJPNJTxIj_YH0LfNjVyAbaMGGf4m6a7QgfWOUBXcmUuFjA&q=chair&sa=X&ved=2ahUKEwj8kfe0-5SSAxVfQUEAHaD_A_AQtKgLegQIExAB&biw=1890&bih=955&dpr=1.6&aic=0"
+          target="_blank"
+        >
+          here
+        </a>
+        .
+      </p>
+      <p>
+        There isn't much you can do on this website but there is a textbox that
+        you can enter a secret into and you can just get it out of your system.
+      </p>
+      <p>
+        I don't know <i>how</i> to make textboxes that save responses.
+      </p>
+      <p>
+        {" "}
+        Even if I did know I would keep this as it is because I believe many
+        people have said they find the textbox to be useful, cathartic, and a
+        form of light relief that is hard to get in busy urban life or the
+        modern internet. It is free to use and you can use as much or as little
+        as you like.
+      </p>
+      <form className="secretTextbox">
+        <input></input>
+      </form>
+      <p>
+        If you would like to give me more feedback on the secret textbox here is
+        another textbox that you can use.
+      </p>
+      <form className="secretTextbox">
+        <input></input>
+      </form>
+      <p>
+        Follow Joe on{" "}
+        <a href="https://www.instagram.com/joevaughanleaks">Instagram</a> or{" "}
+        <a href="https://bsky.app/profile/joevaughan.net">Bluesky</a>.
+      </p>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <head>
+      <title>Joe Vaughan Is Embracing The Future</title>
+    </head>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
